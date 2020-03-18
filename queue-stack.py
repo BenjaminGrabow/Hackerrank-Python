f5e89b32ac8c1dae290cfa9789ffbdb9f2d74d66
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-03-18 23:12:53 +0100

    create queue and stack

--- queue-stack.py
+++ queue-stack.py
@@ -1,4 +1,32 @@
+# Queue is a FIFO (first in first out) 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
 
 # Stack is a LIFO (last in first out)
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
 
-# Queue is a FIFO (first in first out) 
+
+
+
+
