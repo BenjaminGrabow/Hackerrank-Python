3f27d190f61174c9e7b511eac929d0bf6300d734
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-03-18 23:11:06 +0100

    change bfs to dfs

--- Count_Luck.py
+++ Count_Luck.py
@@ -1,18 +1,18 @@
 # https://www.hackerrank.com/challenges/count-luck/problem
 
-class Queue():
+class Stack():
     def __init__(self):
-        self.queue = []
-    def enqueue(self, value):
-        self.queue.append(value)
-    def dequeue(self):
-        if self.size():
-            return self.queue.pop(0)
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
         else:
             return None
     def size(self):
-        return len(self.queue)
-# Complete the countLuck function below.
+        return len(self.stack)
+
 def countLuck(matrix, k):
     rows = len(matrix)
     cols = len(matrix[0])
@@ -23,6 +23,7 @@ def countLuck(matrix, k):
     path = None
     neighbours = {}
     counter = 0
+
     for row in range(rows):
         for col in range(cols):
             if matrix[row][col] == 'M':
@@ -32,29 +33,34 @@ def countLuck(matrix, k):
                 fr = row
                 fc = col
             neighbours[(row, col)] = neighbour_counter(matrix, row, col, rows, cols)
-    path = bfs(matrix, sr, sc, fr, fc, rows, cols)
+    path = dfs(matrix, sr, sc, fr, fc, rows, cols)
     if neighbours[(sr, sc)] > 1:
       counter += 1
     for coordinates in path:
       if neighbours[coordinates] > 2 and coordinates != (sr, sc) and coordinates != (fr, fc):
         counter += 1
+
     if counter == k:
         return 'Impressed'
     else:
         return 'Oops!' 
-def bfs(matrix, sr, sc, fr, fc, rows, cols):
-    queue = Queue()
-    queue.enqueue([(sr, sc)])
+
+
+
+def dfs(matrix, sr, sc, fr, fc, rows, cols):
+    stack = Stack()
+    stack.push([(sr, sc)])
     visited = set([(sr, sc)])
-    while queue.size():
-        path = queue.dequeue()
+    while stack.size():
+        path = stack.pop()
         row, col = path[-1]
         if row == fr and col == fc:
             return path
         for row2, col2 in ((row + 1, col), (row, col + 1), (row - 1, col), (row, col - 1)):
             if 0 <= row2 < rows and 0 <= col2 < cols and matrix[row2][col2] != 'X' and (row2, col2) not in visited:
-                queue.enqueue(path + [(row2, col2)])
+                stack.push(path + [(row2, col2)])
                 visited.add((row2, col2))
+
 def neighbour_counter(matrix, row, col, rows, cols):
   counter = 0
   if row > 0 and matrix[row - 1][col] != 'X':
